5abbba97f74a1bde875dbffb71f94fe4fb4a0f34
DavidHearl <david.hearl@outlook.com>
2023-09-15 12:52:54 +0100

    added initial html page

--- front_end/views.py
+++ front_end/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def say_hello():
+    return render(request, 'front_end/front_end.html')
