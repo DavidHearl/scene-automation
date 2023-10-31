0b7d0dd6052853c9a06fbf454bae7339232b9013
DavidHearl <david.hearl@outlook.com>
2023-10-31 17:14:09 +0000

    added basic html for the rest of the database values, data will need to be sorted

--- front_end/views.py
+++ front_end/views.py
@@ -4,9 +4,13 @@ from .models import Ship, Area, Status
 
 def say_hello(request):
     ships = Ship.objects.all()
+    areas = Area.objects.all()
+    status = Status.objects.all()
 
     context = {
-        'ships': ships
+        'ships': ships,
+        'areas': areas,
+        'status': status,
     }
 
     return render(request, 'front_end/front_end.html', context)
