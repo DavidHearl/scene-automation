5ec53e657e894248e070e1010ec12f4750299297
DavidHearl <david.hearl@outlook.com>
2023-11-03 12:56:56 +0000

    added separate pages for adding ships and areas

--- front_end/forms.py
+++ front_end/forms.py
@@ -1,54 +1,13 @@
 from django import forms
 from .models import Ship, Area
 
-
-STATUS_CHOICES = [
-    ("No Data", "No Data"),
-    ("Legacy", "Legacy"),
-    ("Failed", "Failed"),
-    ("Queued", "Queued"),
-    ("WIP", "WIP"),
-    ("Completed", "Completed")
-]
+class ShipForm(forms.ModelForm):
+    class Meta:
+        model = Ship
+        fields = ['name', 'contract_number', 'company']
 
 
-class AddItemForm(forms.Form):
-    ship = forms.ModelChoiceField(
-        queryset=Ship.objects.all(),
-        required=False,
-        empty_label="Create New Ship",
-        widget=forms.Select(attrs={'class': 'form-control', 'id': 'ship-select'}),
-    )
-    area_name = forms.CharField(max_length=100)
-
-    # Fields for creating a new ship
-    custom_ship_name = forms.CharField(max_length=200)
-    custom_company = forms.CharField(max_length=200)
-    custom_contract_number = forms.IntegerField()
-    
-    imported = forms.ChoiceField(choices=STATUS_CHOICES, initial="No Data")
-    processed = forms.ChoiceField(choices=STATUS_CHOICES, initial="No Data")
-    registered = forms.ChoiceField(choices=STATUS_CHOICES, initial="No Data")
-    aligned = forms.ChoiceField(choices=STATUS_CHOICES, initial="No Data")
-    cleaned = forms.ChoiceField(choices=STATUS_CHOICES, initial="No Data")
-    point_cloud = forms.ChoiceField(choices=STATUS_CHOICES, initial="No Data")
-    exported = forms.ChoiceField(choices=STATUS_CHOICES, initial="No Data")
-    uploaded = forms.ChoiceField(choices=STATUS_CHOICES, initial="No Data")
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['custom_ship_name'].required = False
-        self.fields['custom_company'].required = False
-        self.fields['custom_contract_number'].required = False
-
-    def clean(self):
-        cleaned_data = super().clean()
-        selected_ship = cleaned_data.get('ship')
-
-        if not selected_ship:
-            # If "Create New Ship" is selected, check if any of the custom fields is empty
-            for field_name in ['custom_ship_name', 'custom_company', 'custom_contract_number']:
-                if not cleaned_data.get(field_name):
-                    self.add_error(field_name, "This field is required when 'Create New Ship' is selected.")
-
-        return cleaned_data
+class AreaForm(forms.ModelForm):
+    class Meta:
+        model = Area
+        fields = '__all__'
--- front_end/views.py
+++ front_end/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Ship, Area
-from .forms import AddItemForm
+from .forms import ShipForm, AreaForm
 
 
 def ships_and_areas(request):
@@ -15,36 +15,28 @@ def ships_and_areas(request):
     return render(request, 'front_end/front_end.html', context)
 
 
-def add_item(request):
+def add_ship(request):
     if request.method == 'POST':
-        form = AddItemForm(request.POST)
-        if form.is_valid():
-            ship = form.cleaned_data['ship']
-            area_name = form.cleaned_data['area_name']
-
-            if not ship:
-                custom_ship_name = form.cleaned_data['custom_ship_name']
-                custom_company = form.cleaned_data['custom_company']
-                custom_contract_number = form.cleaned_data['custom_contract_number']
-                ship = Ship(
-                    name=custom_ship_name,
-                    company=custom_company,
-                    contract_number=custom_contract_number
-                )
-                ship.save()
-
-            area = Area(ship=ship, area_name=area_name)
-            area.save()
-
-            return redirect('success_page')
+        ship_form = ShipForm(request.POST)
+        if ship_form.is_valid():
+            ship = ship_form.save()
+            return redirect('success_page')  # Redirect to a success page or any other desired URL after submission
     else:
-        form = AddItemForm()
+        ship_form = ShipForm()
 
-    context = {
-        'form': form
-    }
+    return render(request, 'front_end/add_ship.html', {'ship_form': ship_form})
 
-    return render(request, 'front_end/add_item.html', context)
+
+def add_area(request):
+    if request.method == 'POST':
+        area_form = AreaForm(request.POST)
+        if area_form.is_valid():
+            area = area_form.save()
+            return redirect('success_page')  # Redirect to a success page or any other desired URL after submission
+    else:
+        area_form = AreaForm()
+
+    return render(request, 'front_end/add_area.html', {'area_form': area_form})
 
 
 def success_page(request):
