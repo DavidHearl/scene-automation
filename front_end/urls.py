d4ed8623a6ef76f5f4d3dfee0f85a4ac1a16f479
DavidHearl <david.hearl@outlook.com>
2023-11-06 11:42:55 +0000

    added the add ship form to the main page for easier access

--- front_end/urls.py
+++ front_end/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_ship/', views.add_ship, name='add_ship'),
     path('add_area/', views.add_area, name='add_area'),
     path('success/', views.success_page, name='success_page'),
 ]
--- front_end/views.py
+++ front_end/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from .models import Ship, Area
 from .forms import ShipForm, AreaForm
 
@@ -7,24 +8,22 @@ def ships_and_areas(request):
     ships = Ship.objects.all()
     areas = Area.objects.all()
 
-    context = {
-        'ships': ships,
-        'areas': areas,
-    }
-
-    return render(request, 'front_end/front_end.html', context)
-
-
-def add_ship(request):
     if request.method == 'POST':
         ship_form = ShipForm(request.POST)
         if ship_form.is_valid():
             ship = ship_form.save()
-            return redirect('success_page')  # Redirect to a success page or any other desired URL after submission
+            messages.success(request, 'Ship added successfully.')
+            return redirect('ships_and_areas')  # Redirect to a success page or any other desired URL after submission
     else:
         ship_form = ShipForm()
 
-    return render(request, 'front_end/add_ship.html', {'ship_form': ship_form})
+    context = {
+        'ships': ships,
+        'areas': areas,
+        'ship_form' : ship_form,
+    }
+
+    return render(request, 'front_end/front_end.html', context)
 
 
 def add_area(request):
