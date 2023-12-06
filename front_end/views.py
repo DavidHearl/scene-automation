5a0c4a93e2aec9731f973c5159b967b7c59a64d5
DavidHearl <david.hearl@outlook.com>
2023-12-06 17:19:41 +0000

    moved calculations from the model.py file to the views.py

--- front_end/views.py
+++ front_end/views.py
@@ -6,6 +6,75 @@ from .models import Ship, Area
 from .forms import ShipForm, AreaForm
 
 
+# Time in minutes
+hours_per_workday = 8
+time_per_scan = 20
+time_per_area = 60
+minor_error_time = 15
+major_error_time = 30
+critical_error_time = 45
+time_per_area_failed = 30
+
+
+def calculate_completed_percentage(ship):
+    # Move the completed_percentage function logic here
+    ship_total_scans = ship.total_scans()
+
+    if ship_total_scans == 0:
+        return 0
+
+    percentage = 0
+
+    for area in ship.area_set.all():
+        weighting = 0
+        process_stage = ["imported", "processed", "registered", "aligned", "cleaned", "point_cloud", "exported", "uploaded"]
+        process_weighting = [12.5, 100, 100, 12.5, 75, 100, 500, 100]
+
+        for i, status in enumerate(process_stage):
+            if getattr(area, status) == "Completed" or getattr(area, status) == "Legacy" or getattr(area, status) == "No Data":
+                weighting += process_weighting[i]
+
+        area_percentage = (100 * (weighting/1000) * (int(area.scans) / int(ship_total_scans)))
+        percentage += area_percentage
+
+    return round(percentage, 1)
+
+
+def calculate_estimated_completion(ship):
+    total_scans = ship.total_scans()
+    completed_percentage = calculate_completed_percentage(ship)
+    estimated_time_per_scan = total_scans * time_per_scan / (60 * hours_per_workday)
+    total_areas = ship.area_set.count()
+    additional_time_per_area = total_areas * (time_per_area / (60 * hours_per_workday))
+    total_failure_time = 0
+    number_of_failures = 0
+
+    for area in ship.area_set.all():
+        if area.registered == "Minor Fail":
+            total_failure_time += int(area.scans) * minor_error_time
+            number_of_failures += 1
+        elif area.registered == "Major Fail":
+            total_failure_time += int(area.scans) * major_error_time
+            number_of_failures += 1
+        elif area.registered == "Critical Fail":
+            total_failure_time += int(area.scans) * critical_error_time
+            number_of_failures += 1
+
+    total_area_fail_time = (number_of_failures * time_per_area_failed) / (60 * hours_per_workday)
+    total_failure_time = (total_failure_time / (60 * hours_per_workday)) + total_area_fail_time
+    estimated_time = estimated_time_per_scan + additional_time_per_area + total_failure_time
+    multiplier = 1 - (completed_percentage / 100)
+    estimated_time *= 0 if multiplier == 0 else multiplier
+
+    return round(estimated_time, 2)
+
+
+def total_estimated_completion_for_all_ships():
+    # Iterate over all ships and sum their estimated completion times
+    total_estimated_time_for_all_ships = sum(calculate_estimated_completion(ship) for ship in Ship.objects.all())
+    return round(total_estimated_time_for_all_ships, 2)
+
+
 def ships_and_areas(request):
     # Calculate the total scans for all areas
     total_scans = Area.objects.aggregate(total_scans=Sum('scans'))['total_scans'] or 0
@@ -23,7 +92,7 @@ def ships_and_areas(request):
     avg_completion_time = round(avg_scans_per_ship * 20)
     avg_completion_time = round(avg_completion_time / (60 * 8), 1)
 
-    total_estimated_completion_for_all_ships = Ship.total_estimated_completion_for_all_ships()
+    total_estimated_completion = total_estimated_completion_for_all_ships()
 
     ships = Ship.objects.all()
     areas = Area.objects.all()
@@ -48,8 +117,14 @@ def ships_and_areas(request):
     else:
         area_form = AreaForm()
 
+    for ship in ships:
+        ship.completed_percentage = calculate_completed_percentage(ship)
+        ship.estimated_completion = calculate_estimated_completion(ship)
+        print(f"Ship: {ship.name}, Completed Percentage: {ship.completed_percentage}, Estimated Completion: {ship.estimated_completion}")
+
     context = {
         'ships': ships,
+        'completed_percentages': [ship.completed_percentage for ship in ships],
         'areas': areas,
         'total_scans': total_scans,
         'num_ships': num_ships,
@@ -57,7 +132,7 @@ def ships_and_areas(request):
         'avg_areas_per_ship': avg_areas_per_ship,
         'avg_scans_per_ship': avg_scans_per_ship,
         'avg_completion_time': avg_completion_time,
-        'total_estimated_completion_for_all_ships': total_estimated_completion_for_all_ships,
+        'total_estimated_completion': total_estimated_completion,
         'ship_form' : ship_form,
         'area_form' : area_form,
     }
