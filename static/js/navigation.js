59c85cfdeab39915788b6af9f3bb210b3a9474c3
DavidHearl <david.hearl@outlook.com>
2024-06-26 11:57:45 +0100

    added mobile navigation

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -11,10 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
     function applyNavigationSize(size) {
         if (size === 'large') {
             navigation.style.width = '250px';
-            navigationTexts.forEach(text => text.style.display = 'inline');
-            navigationHeadings.forEach(heading => heading.style.justifyContent = 'start');
-            navigationHeadings.forEach(heading => heading.style.fontSize = '1rem');
-            contentContainer.style.marginLeft = '282px';
+            setTimeout(() => {
+                navigationHeadings.forEach(heading => heading.style.justifyContent = 'start');
+            }, 50); 
+            setTimeout(() => {
+                navigationTexts.forEach(text => text.style.display = 'inline');
+                navigationHeadings.forEach(heading => heading.style.fontSize = '1rem');
+                contentContainer.style.marginLeft = '282px';
+            }, 100); 
         } else { // Assume 'small'
             navigation.style.width = '76px';
             navigationTexts.forEach(text => text.style.display = 'none');
@@ -24,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
             navigationHeadings.forEach(heading => {
                 setTimeout(() => {
                     heading.style.justifyContent = 'center';
-                }, 200); // Delay in milliseconds
+                }, 600); // Delay in milliseconds
             });
         }
     }
@@ -40,4 +44,21 @@ document.addEventListener('DOMContentLoaded', function() {
         // Save the new size to localStorage
         localStorage.setItem('navigationSize', newSize);
     });
+
+    // ----------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------
+
+    // Mobile navigation
+    const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+    
+    // Function to toggle mobile navigation
+    function toggleMobileNavigation() {
+        navigation.style.display = navigation.style.display === 'block' ? 'none' : 'block';
+        navigation.style.width = '100%';
+        navigationTexts.forEach(text => text.style.display = 'inline');
+    }
+
+    // Add click event listener to the mobile menu icon
+    mobileMenuIcon.addEventListener('click', toggleMobileNavigation);
 });
