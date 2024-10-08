fda82d2ce97d98d0265b7b5119f0cb25bb9362ea
DavidHearl <david.hearl@outlook.com>
2024-10-08 16:19:56 +0100

    moved all data behind the authentication

--- scene_automation/settings.py
+++ scene_automation/settings.py
@@ -101,7 +101,7 @@ ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 
 LOGIN_URL = '/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'ships_and_areas'
 
 WSGI_APPLICATION = 'scene_automation.wsgi.application'
 
--- scene_automation/urls.py
+++ scene_automation/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf import settings
 from front_end.views import *
 from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', ships_and_areas, name='ships_and_areas'),
+    path('', lambda request: redirect('account_login')),
+    path('ships_and_areas/', ships_and_areas, name='ships_and_areas'),
     path('', include('front_end.urls')),
     path('', include('front_end.urls')),
     path('front_end/', include('front_end.urls')),
