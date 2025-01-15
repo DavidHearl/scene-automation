3f02e3119ef0dd9ec3760f6d595f91974edae98f
DavidHearl <david.hearl@outlook.com>
2025-01-15 16:07:18 +0000

    bolstered the navigation menu in the manuals section

--- manuals/urls.py
+++ manuals/urls.py
@@ -5,5 +5,20 @@ app_name = 'manuals'
 
 urlpatterns = [
     path('', views.manual, name='manual'),
+    # Technical
+    path('folder_structure/', views.folder_structure, name='folder_structure'),
+    path('data_storage/', views.data_storage, name='data_storage'),
+    # Scanner
+    path('scanner_settings/', views.scanner_settings, name='scanner_settings'),
+    path('cleaning_scanner/', views.cleaning_scanner, name='cleaning_scanner'),
+    # Stream
     path('connecting_to_wifi/', views.connecting_to_wifi, name='connecting_to_wifi'),
+    path('file_names/', views.file_names, name='file_names'),
+    # Scene
+    path('scene_settings/', views.scene_settings, name='scene_settings'),
+    path('processing/', views.processing, name='processing'),
+    path('registration/', views.registration, name='registration'),
+    path('cleaning/', views.cleaning, name='cleaning'),
+    path('point_cloud/', views.point_cloud, name='point_cloud'),
+    path('exporting/', views.exporting, name='exporting'),
 ]
--- manuals/views.py
+++ manuals/views.py
@@ -4,8 +4,43 @@ from django.http import HttpResponse
 def manual(request):
     return render(request, 'manuals/manual.html')
 
+# Technical
+def folder_structure(request):
+    return render(request, 'manuals/folder_structure.html')
+
+def data_storage(request):
+    return render(request, 'manuals/data_storage.html')
+
+# Scanner
+def scanner_settings(request):
+    return render(request, 'manuals/scanner_settings.html')
+
+def cleaning_scanner(request):
+    return render(request, 'manuals/cleaning_scanner.html')
+
+# Stream
 def connecting_to_wifi(request):
     return render(request, 'manuals/connecting_to_wifi.html')
 
+def file_names(request):
+    return render(request, 'manuals/file_names.html')
 
+# Scene
+def scene_settings(request):
+    return render(request, 'manuals/scene_settings.html')
+
+def processing(request):
+    return render(request, 'manuals/processing.html')
+
+def registration(request):
+    return render(request, 'manuals/registration.html')
+
+def cleaning(request):
+    return render(request, 'manuals/cleaning.html')
+
+def point_cloud(request):
+    return render(request, 'manuals/point_cloud.html')
+
+def exporting(request):
+    return render(request, 'manuals/exporting.html')
     
